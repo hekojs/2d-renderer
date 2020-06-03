9d22f0864fab1ae9170a96eb9a40a104c49449ef
Anael Favre <anael@sauce-code.fr>
2020-06-03 18:25:08 +0200

    Globalize system and add camera

--- src/Systems/RendererPlugin.js
+++ src/Systems/RendererPlugin.js
@@ -1,13 +1,16 @@
 import Heko from '@hekojs/core'
 
-export default class RendererRenderScene extends Heko.System {
+export default class RendererPlugin extends Heko.System {
   onAdd ({ renderer }) {
     this.renderer = renderer
   }
 
   onTick ({ delta, time }) {
     this.renderer.events.emit('before-tick')
+
     this.renderer.app.ticker.update()
+    this.renderer.camera.onTick()
+
     this.renderer.events.emit('after-tick')
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import Plugin from './Plugin'
 import Heko from '@hekojs/core'
 import Palettes from './Palettes'
 import Components from './Components'
-import Systems from './Systems'
 
 Heko.registerComponents(Components)
 
 export default {
   Components,
-  Systems,
-  Palettes: {
-    Heko
-  },
+  Palettes,
   Plugin
 }
